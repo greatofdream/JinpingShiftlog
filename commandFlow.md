6cce32fcd7ad856d002ae877991ef62cef2ee258
greatofdream <greatofdream@gmail.com>
2020-08-28 21:32:25 +0800

    add goodrunlist

--- commandFlow.md
+++ commandFlow.md
@@ -33,3 +33,12 @@ vim config/config.txt
 ./beginrun.csh
 ```
 ## Go Back
+
+## DetectorSummary
+`名称管理器` set variables
+such as:
++ DarkR=OFFSET(Data!$I$1,MATCH(MAX(Data!$A:$A)+1,Data!$A:$A)-Plot!$A$2,0,Plot!$A$2)
+
+MATCH(lookup_value, lookup_array, [match_type])
+[match](https://support.microsoft.com/zh-cn/office/match-%e5%87%bd%e6%95%b0-e8dffd45-c762-47d6-bf89-533f4a37673a?ui=zh-cn&rs=zh-cn&ad=cn)
+[offset](https://support.microsoft.com/zh-cn/office/offset-%E5%87%BD%E6%95%B0-c8de19ae-dd79-4b9b-a14e-b4d906d11b66)
--- shiftXlsToTxt.py
+++ shiftXlsToTxt.py
@@ -2,10 +2,11 @@ import pandas as pd, argparse
 psr = argparse.ArgumentParser()
 psr.add_argument('-i', dest="input", help="input xlsx file")
 psr.add_argument('-o', dest="output", help="output txt file")
+psr.add_argument('-O', dest='goodrun', help='goodrun list file')
 args = psr.parse_args()
 
 xlsx = pd.read_excel(args.input, sheet_name='shiftLog', index_col=None, na_values=['NA'])
-with open(args.output, 'w') as opt:
+with open(args.output, 'w') as opt, open(args.goodrun, 'w') as opt2:
     for index, row in xlsx.iterrows():
         #print(row)
         opt.write('{}\n'.format(row['Date'].strftime('%Y-%m-%d')))
@@ -19,5 +20,9 @@ with open(args.output, 'w') as opt:
         opt.write('{} start phy run {}\n'.format(row['PhyStartTime'].strftime('%H:%M'), row['PhyRun']))
         opt.write('\n')
         opt.write('Nitrogen input: {:.2f} Mpa\nNitrogen output: {:.2f} Mpa\nFlux: {:.2f} sccm\nInflation: {}\n'.format(row['N2In'], row['N2Out'], row['flux'], row['inflation']))
-        opt.write('Oxygen: {:.1f}%, {:.1f} degree\n'.format(row['Oxygen'], row['Temperature']))
+        opt.write('Oxygen: {:.1f}%, {:.1f} degree\n\n'.format(row['Oxygen'], row['Temperature']))
+        if not pd.isna(row['Accident']):
+            opt.write('Accident:\n{}\n'.format('\n'.join(str(row['Accident']).split(';'))))
         opt.write('\n')
+        opt2.write('{}\n'.format(row['PhyRun']))
+    opt2.write('\n')
